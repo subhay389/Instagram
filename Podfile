a98507a7473fe1a7ee7b6a1dc52d5db038165315
Subhay Manandhar <subhay389@hotmail.com>
2017-03-21 18:55:41 -0400

    final

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'instagram' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for instagram
+    pod ‘Parse’
+    pod ‘ParseUI’
+    inhibit_all_warnings!
+    pod 'MBProgressHUD'
+
+  target 'instagramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'instagramUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ instagram/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  instagram
+//
+//  Created by Subhay Manandhar on 3/21/17.
+//  Copyright © 2017 Subhay Manandhar. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logOut(_ sender: Any) {
+        PFUser.logOut()
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "UserDidLogOut"), object: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
